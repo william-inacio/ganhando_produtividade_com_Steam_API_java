36e20eb5d42e52d1c53068acaf113ceb775819d4
william-inacio <william.inacio02@gmail.com>
2023-09-22 21:13:18 -0300

    conhecendo steam API, curso java da DIO
    Signed-off-by: william-inacio <william.inacio02@gmail.com>

---
+++ src/ConsumerExample.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 8);
+
+        Consumer<Integer> imprimirNumeroPar = numero -> {
+            if (numero % 2 == 0) {
+                System.out.println(numero);
+            }
+        };
+
+        numeros.forEach(n -> {
+                    if (n % 2 == 0) {
+                        System.out.println(n);
+                    }
+                }
+        );
+    }
+}
